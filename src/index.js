349283870d82085066af38d1fa964ce60467c799
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-14 18:47:16 -0500

    Added timestamp path and middleware to serve index.html

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
+const path = require('path')
 const express = require('express')
-
+const { timestamp } = require('./timestamp')
 const app = express()
 
+const indexPath = path.join(__dirname, '../', 'public', 'index.html')
+
+app.use('/', express.static(path.normalize(indexPath)))
+
 app.get('/', (req,res) => {
-    res.send('You\'ve reached the timestamp microservice homepage.')
+    res.sendFile(path.normalize(indexPath))
+})
+
+app.get('/:timestamp', (req, res) => {
+    const ts = req.params.timestamp
+    res.json(timestamp(ts))
 })
 
 app.listen(8000, () => {
